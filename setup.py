6c337468ae34d6d43077d86c3f7845a13effadaa
Vincent Le <vle@securitycompass.com>
2019-12-03 13:16:47 -0500

    Added Python version to classifier for setup.py

--- setup.py
+++ setup.py
@@ -17,6 +17,12 @@ setup(
     classifiers=[
         'Intended Audience :: Developers',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Operating System :: OS Independent',
         'Environment :: Web Environment',
         'Framework :: Django',
